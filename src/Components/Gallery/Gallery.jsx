cebc0cfa3c8042fe7b6bb8c2e66b9632cc658cf9
thakur7071 <singhnaveen2370@gmail.com>
2025-02-20 13:11:25 +0530

    Initial Commit

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,11 +1,39 @@
-
+import { useState } from "react";
+import "./gallery.css";
 
 const Gallery = () => {
-  return (
-    <div>
-      gallery hu 
-    </div>
-  )
-}
+  const [selectedImage, setSelectedImage] = useState(null);
 
-export default Gallery
+  const images = [
+    "/src/Components/Assets/mainimg.jpg",
+  "/src/Components/Assets/img10.jpg",
+  "/src/Components/Assets/img5.jpg",
+  "/src/Components/Assets/img9.jpg",
+  "/src/Components/Assets/img8.jpg",
+  "/src/Components/Assets/img2.jpg",
+  "/src/Components/Assets/img3.jpg",
+  "/src/Components/Assets/img1.jpg",
+
+  ];
+
+  return (
+    <div className="gallery-container">
+      {images.map((src, index) => (
+        <div className="gallery-item" key={index} onClick={() => setSelectedImage(src)}>
+          <img src={src} alt="Gallery" loading="lazy" />
+          <div className="overlay"></div>
+        </div>
+      ))}
+
+      {/* Fullscreen Modal */}
+      {selectedImage && (
+        <div className="modal" onClick={() => setSelectedImage(null)}>
+          <img src={selectedImage} alt="Full Size" className="modal-img" />
+          <span className="close">&times;</span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Gallery;
